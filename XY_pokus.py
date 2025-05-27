a415802d1c37be060aacab32c3736a086f66a8c1
elinkakafkyc <eli.kafkova@seznam.cz>
2025-05-27 11:57:04 +0200

    autori

--- XY_pokus.py
+++ XY_pokus.py
@@ -45,6 +45,8 @@ st.sidebar.markdown("[ **🗝️ Streamlit appka!**](https://github.com/elinkaka
 
 # NADPIS
 st.title("Metr za milion: Mileniálské dilema")
+st.subheader("*Eliška Kafková & Michaela Kaderová  " \
+"Mentoři: Eliška Valterová & Jakub Červinka")
 
 # ÚVOD
 st.header("Úvod")
