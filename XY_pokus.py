6f0dafd5db68d47a98ada173af1238259e26d6fa
elinkakafkyc <eli.kafkova@seznam.cz>
2025-05-29 22:39:05 +0200

    sql kodik fix

--- XY_pokus.py
+++ XY_pokus.py
@@ -193,7 +193,7 @@ st.write()
 st.caption("*Výpočet průměrné ceny na m² dle městské části v roce 2024.*")   
 
 code = '''
--- SQL dotaz na mzdy podle pohlaví a věku
+
 SELECT
     "MC_spravne",
     ROUND(AVG("data_price"/"plocha")) AS "prumerna_cena_na_m2"
