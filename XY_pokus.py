84eb63807b3e6216af60659b1e6cf4e75c1748fe
elinkakafkyc <eli.kafkova@seznam.cz>
2025-05-31 20:34:22 +0200

    netusim

--- XY_pokus.py
+++ XY_pokus.py
@@ -86,7 +86,14 @@ barvy = {
 }
 
 # vyber barvy
-vybrana_barva = st.sidebar.radio("**Prohlídni si barvy z naší palety!**", list(barvy.keys()), horizontal=True)
+vybrana_barva = st.sidebar.radio("###**Prohlídni si barvy z naší palety!**", list(barvy.keys()), horizontal=True)
+st.sidebar.markdown("### Celá paleta")
+for barva in barvy.values():
+    border = "3px solid black" if barva == barvy else "1px solid #ccc"
+    st.markdown(
+        f'<div style="display:inline-block; width:50px; height:50px; background-color:{barva}; border:{border}; margin:5px; border-radius:5px;"></div>',
+        unsafe_allow_html=True
+    )
 
 # ziskani kodu
 hex_kod = barvy[vybrana_barva]
